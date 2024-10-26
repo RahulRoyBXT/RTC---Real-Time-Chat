77b1f9fa0e23933d37526687af71fd2dfb638390
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2024-10-27 00:27:31 +0530

    Auto Added UserName

--- src/components/Zego.jsx
+++ src/components/Zego.jsx
@@ -1,13 +1,15 @@
-
+import { auth } from "../firebase";
 import './Zego.css';
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 const RoomPage = () => {
+    const  displayName  = auth.currentUser.displayName;
+    
     const { roomID } = useParams();
     const myMeeting = async (element) => {
         const appID = 1155155914;
         const serverSecret = "71cfe2df910966c1825ee685ce77900a";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "Shiv");
+        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), displayName);
         const zc = ZegoUIKitPrebuilt.create(kitToken);
         zc.joinRoom({
             container: element,
