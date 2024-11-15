7c9b03499915877943074fd630798f31839273f5
Rahul Roy <rahulroy.it2021@nsec.ac.in>
2024-11-15 23:31:00 +0530

    added Animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,30 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import Zego from './components/Zego';
 import useCleanupRooms from './hooks/useCleanupRooms'
+import { AnimatedBackground } from 'animated-backgrounds';
+import { useEffect, useState } from 'react';
 
 function App() {
   if (import.meta.env.MODE === 'development') {
     console.log("App is running in development mode.");
   }
   useCleanupRooms();
-  return (
+
+  // const [animationName, setAnimationName] = useState('starryNight');
+  // useEffect(() => {
+  //   const animations = ['rainbowWaves', 'floatingBubbles', 'gradientWave', 'particleNetwork', 'galaxySpiral'];
+  //   const storedIndex = localStorage.getItem('backgroundAnimationIndex');
+  //   const newIndex = storedIndex ? (parseInt(storedIndex) + 1) % animations.length : 0;
+  //   setAnimationName(animations[newIndex]);
+  //   localStorage.setItem('backgroundAnimationIndex', newIndex.toString());
+  // }, []);
+  return (<>
+    <AnimatedBackground animationName='auroraBorealis'/>
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/room/:roomID' element={<Zego />} />
     </Routes>
+    </>
   );
 }
 
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,53 +1,122 @@
-import { useState } from 'react'
-import { auth, db } from './firebase'
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Chat from './components/Chat';
-import RoomForm from './components/RoomForm';
-import useAuth from './hooks/useAuth';
+import { useState, useEffect } from "react";
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Chat from "./components/Chat";
+import RoomForm from "./components/RoomForm";
+import useAuth from "./hooks/useAuth";
+import { motion, useAnimationControls } from "framer-motion";
 
 function HomePage() {
   const [user] = useAuthState(auth);
-  const [currentRoom, setCurrentRoom] = useState('');
-  const { googleSignIn,signOut } = useAuth();
-  
+  const [currentRoom, setCurrentRoom] = useState("");
+  const { googleSignIn, signOut } = useAuth();
+
+  // Animation Controls
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    const loop = async () => {
+      let isActive = true;
+      while (isActive) {
+        await controls.start("visible");
+        await new Promise((resolve) => setTimeout(resolve, 8000)); // Delay 10 seconds
+      }
+      return () => {
+        isActive = false;
+      };
+    };
+    loop();
+  }, [controls]);
+
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3, // Stagger animation for children
+      },
+    },
+  };
+
+  const textVariants = {
+    hidden: { opacity: 0, y: "-10%" },
+    visible: { opacity: 1, y: "0%", transition: { duration: 1 } },
+  };
+
   return (
-    <div className='max-sm:bg-gray-200 max-sm:dark:bg-white w-full min-h-screen'>
-      <div className='flex flex-col items-center gap-5 pt-10'>
-        <h1 className='sm:hidden text-4xl font-bold text-transparent bg-clip-text text-center'>
-         <span className='text-5xl font-extrabold text-purple-600'> RTC </span> <br /><span className='text-xl text-pink-900'> Real Time Chat </span>
+    <div className="w-full min-h-screen">
+      {/* Header */}
+      <div className="flex flex-col items-center gap-5 pt-10">
+        <h1 className="sm:hidden text-4xl font-bold text-transparent bg-clip-text text-center">
+          <span className="text-5xl font-extrabold text-purple-600">RTC</span>
+          <br />
+          <span className="text-xl text-pink-900">Real Time Chat</span>
         </h1>
-        <h1 className='max-sm:hidden text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-600 to-pink-900'>
+        <h1 className="max-sm:hidden text-6xl font-bold text-transparent text-[#00C8AB]">
           RTC - Real Time Chat
         </h1>
-        {
-          user && <h2>Welcome {user.displayName}</h2>
-        }
-        {
-          user ? (
-            <button onClick={signOut} className='border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]'>
-              Sign Out
-            </button>
-          ) : (
-            <button onClick={googleSignIn} className='border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]'>
-              Google Sign In
-            </button>
-          )
-        }
+        {user && <h2>Welcome {user.displayName}</h2>}
+        {/* Sign In/Out Button */}
+        {user ? (
+          <button
+            onClick={signOut}
+            className="bg-[#0370E9] border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <motion.button
+            whileHover={{ scale: 1.5 }}
+            whileTap={{ scale: 0.95 }}
+            whileDrag={{ scale: 0.9, rotate: 10 }}
+            drag
+            onClick={googleSignIn}
+            className="bg-[#0370E9] border-black border-2 rounded-xl font-bold p-4 text-center w-[13rem]"
+          >
+            Google Sign In
+          </motion.button>
+        )}
       </div>
-      {
-        !user && <><div className='sm:p-20 sm:mt-10 max-sm:mt-20 w-full h-full bg-gradient-to-r from-purple-600 to-red-800 font-bold text-5xl text-transparent bg-clip-text  max-sm:p-5 max-sm:text-6xl'>
-         <span className='sm:text-6xl sm:leading-loose'> Welcome</span><br /><span className='sm:text-5xl'> to the</span> <span className='sm:text-red-800'> RTC</span>,  <span className='max-sm:text-4xl'><span> <br/> </span><span className='sm:text-red-800'>Sign In</span> to Continue.</span>
-        </div>
-        </>
-      }
-      {/* {
-        user && currentRoom && <Chat setCurrentRoom={setCurrentRoom} room={currentRoom} />
-      } */}
-      {
-        user && !currentRoom && <RoomForm setCurrentRoom={setCurrentRoom} />
-      }
+
+      {/* Animation Section */}
+      {!user && (
+        <motion.div
+          className="overflow-hidden sm:p-20 sm:mt-10 max-sm:mt-20 w-full h-full bg-gradient-to-r from-purple-600 to-red-800 font-bold text-5xl text-transparent bg-clip-text max-sm:p-5 max-sm:text-6xl"
+          animate={controls}
+          initial="hidden"
+          variants={containerVariants}
+        >
+          <motion.span className="sm:text-8xl" variants={textVariants}>
+            Welcome
+          </motion.span>
+          <br />
+          <motion.span className="sm:text-5xl" variants={textVariants}>
+            to the
+          </motion.span>{" "}
+          <motion.span className="sm:text-red-800" variants={textVariants}>
+            RTC
+          </motion.span>
+          ,{" "}
+          <motion.span className="max-sm:text-4xl" variants={textVariants}>
+            <span>
+              {" "}
+              <br />{" "}
+            </span>
+            <motion.span className="sm:text-red-800" variants={textVariants}>
+              Sign In
+            </motion.span>{" "}
+            to Continue.
+          </motion.span>
+        </motion.div>
+      )}
+
+      {/* Room Form or Chat */}
+      {user && !currentRoom && <RoomForm setCurrentRoom={setCurrentRoom} />}
+      {user && currentRoom && (
+        <Chat setCurrentRoom={setCurrentRoom} room={currentRoom} />
+      )}
     </div>
-  )
+  );
 }
 
 export default HomePage;
